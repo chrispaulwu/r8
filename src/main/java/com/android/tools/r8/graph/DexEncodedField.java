4e7b7d6218d6e2c6058a05c5a7fbf21e25559eec
Morten Krogh-Jespersen <mkroghj@google.com>
2022-09-27 17:59:52 +0200

    [Metadata] Copy kotlin metadata when type substituting a field
    Bug: b/245724176
    Change-Id: I7703d7d98a665fc6fdf58cae2788282330d10b1d

--- src/main/java/com/android/tools/r8/graph/DexEncodedField.java
+++ src/main/java/com/android/tools/r8/graph/DexEncodedField.java
@@ -21,6 +21,7 @@ import com.android.tools.r8.ir.optimize.info.FieldOptimizationInfo;
 import com.android.tools.r8.ir.optimize.info.MutableFieldOptimizationInfo;
 import com.android.tools.r8.ir.optimize.info.OptimizationFeedbackSimple;
 import com.android.tools.r8.kotlin.KotlinFieldLevelInfo;
+import com.android.tools.r8.kotlin.KotlinMetadataUtils;
 import com.android.tools.r8.shaking.AppInfoWithLiveness;
 import com.android.tools.r8.utils.ConsumerUtils;
 import com.android.tools.r8.utils.structural.StructuralItem;
@@ -400,6 +401,7 @@ public class DexEncodedField extends DexEncodedMember<DexEncodedField, DexField>
     private DexAnnotationSet annotations = DexAnnotationSet.empty();
     private FieldAccessFlags accessFlags;
     private FieldTypeSignature genericSignature = FieldTypeSignature.noSignature();
+    private KotlinFieldLevelInfo kotlinInfo = KotlinMetadataUtils.getNoKotlinInfo();
     private DexValue staticValue = null;
     private ComputedApiLevel apiLevel = ComputedApiLevel.notSet();
     private FieldOptimizationInfo optimizationInfo = DefaultFieldOptimizationInfo.getInstance();
@@ -419,8 +421,8 @@ public class DexEncodedField extends DexEncodedMember<DexEncodedField, DexField>
       // Copy all the mutable state of a DexEncodedField here.
       field = from.getReference();
       accessFlags = from.accessFlags.copy();
-      // TODO(b/169923358): Consider removing the fieldSignature here.
       genericSignature = from.getGenericSignature();
+      kotlinInfo = from.getKotlinInfo();
       annotations = from.annotations();
       staticValue = from.staticValue;
       apiLevel = from.getApiLevel();
@@ -521,6 +523,7 @@ public class DexEncodedField extends DexEncodedMember<DexEncodedField, DexField>
               apiLevel,
               deprecated,
               d8R8Synthesized);
+      dexEncodedField.setKotlinMemberInfo(kotlinInfo);
       dexEncodedField.optimizationInfo = optimizationInfo;
       buildConsumer.accept(dexEncodedField);
       return dexEncodedField;
