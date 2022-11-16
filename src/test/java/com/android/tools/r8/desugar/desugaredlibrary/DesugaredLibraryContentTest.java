217c7a0ad5ea469b596d372693fa31c4981fcf29
Clément Béra <clementbera@google.com>
2022-11-16 10:50:13 +0100

    New desugared library nio FileChannel implementation
    - Maintain prefix for SeekableByteChannel to avoid conversions
    - Custom management of FileChannel on all Apis, but no rewritting or
      or automatic wrappers for FileChannel
    - Emulation of custom FileChannel with RandomAccessFile below 26
    - Wrap FileChannel below 24 to support SeekableByteChannel interface on
      library boundaries
    
    Above 24 the FileChannel should be 100% supported, below 24 there may be some locking issues to address [isOpen(), close(), begin(), end(boolean)]
    
    Change-Id: I11ea847f97f0317b444e595cc152843d0df805dc

--- src/test/java/com/android/tools/r8/desugar/desugaredlibrary/DesugaredLibraryContentTest.java
+++ src/test/java/com/android/tools/r8/desugar/desugaredlibrary/DesugaredLibraryContentTest.java
@@ -26,9 +26,11 @@ import com.android.tools.r8.utils.codeinspector.CodeInspector;
 import com.android.tools.r8.utils.codeinspector.FoundMethodSubject;
 import com.android.tools.r8.utils.codeinspector.InstructionSubject;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assume;
 import org.junit.Test;
@@ -38,6 +40,34 @@ import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public class DesugaredLibraryContentTest extends DesugaredLibraryTestBase {
+
+  // The class sun.misc.Unsafe is runnable on Android despite not being in android.jar.
+  private static final Set<String> ALLOWED_MISSING_HOLDER = ImmutableSet.of("sun.misc.Unsafe");
+  private static final Set<String> ALLOWED_MISSING_METHOD =
+      ImmutableSet.of(
+          // The takeWhile/dropWhile methods are present in the wrappers but not yet present on
+          // android.jar
+          // leading to NoSuchMethod errors, yet, we keep them for subsequent android versions.
+          "java.util.stream.IntStream"
+              + " java.util.stream.IntStream.dropWhile(java.util.function.IntPredicate)",
+          "java.util.stream.Stream java.util.stream.Stream.takeWhile(java.util.function.Predicate)",
+          "java.util.stream.LongStream"
+              + " java.util.stream.LongStream.dropWhile(java.util.function.LongPredicate)",
+          "java.util.stream.DoubleStream"
+              + " java.util.stream.DoubleStream.takeWhile(java.util.function.DoublePredicate)",
+          "java.util.stream.IntStream"
+              + " java.util.stream.IntStream.takeWhile(java.util.function.IntPredicate)",
+          "java.util.stream.Stream java.util.stream.Stream.dropWhile(java.util.function.Predicate)",
+          "java.util.stream.LongStream"
+              + " java.util.stream.LongStream.takeWhile(java.util.function.LongPredicate)",
+          "java.util.stream.DoubleStream"
+              + " java.util.stream.DoubleStream.dropWhile(java.util.function.DoublePredicate)",
+          // FileStore.getBlockSize() was added in 33 which confuses the required library (30).
+          "long java.nio.file.FileStore.getBlockSize()",
+          // The call is present but unreachable above 26.
+          "java.nio.channels.FileChannel"
+              + " java.nio.channels.DesugarChannels.openEmulatedFileChannel(java.nio.file.Path,"
+              + " java.util.Set, java.nio.file.attribute.FileAttribute[])");
 
   private final TestParameters parameters;
   private final CompilationSpecification compilationSpecification;
