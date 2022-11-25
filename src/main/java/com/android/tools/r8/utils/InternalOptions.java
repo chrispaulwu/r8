63949a1b6ff420b3f2135adb48d4e7f5989dc260
Morten Krogh-Jespersen <mkroghj@google.com>
2022-11-25 15:05:11 +0100

    [ApiModel] Allow stubbing of classes if directly set by property
    Bug: b/259076765
    Change-Id: I95a5fc6532603e028a301b76cb98879d1caf2d37

--- src/main/java/com/android/tools/r8/utils/InternalOptions.java
+++ src/main/java/com/android/tools/r8/utils/InternalOptions.java
@@ -1721,6 +1721,9 @@ public class InternalOptions implements GlobalKeepInfoConfiguration {
         System.getProperty("com.android.tools.r8.disableApiModeling") == null;
     public boolean enableOutliningOfMethods =
         System.getProperty("com.android.tools.r8.disableApiModeling") == null;
+    // TODO(b/259076765): Remove when resolved.
+    public boolean stubNonThrowableClasses =
+        System.getProperty("com.android.tools.r8.stubNonThrowableClasses") != null;
 
     // TODO(b/232823652): Enable when we can compute the offset correctly.
     public boolean useMemoryMappedByteBuffer = false;
