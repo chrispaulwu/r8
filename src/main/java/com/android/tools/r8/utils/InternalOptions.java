57a11e5142249bde710d4a92c87d0b7612d0328a
Søren Gjesse <sgjesse@google.com>
2023-03-03 08:47:37 +0100

    Also apply the rewriting for JDK-8272564 up to Andorid R
    Bug: b/271408544
    Change-Id: I240de7d301677668b9c13b3b49c7b7951e6434b6

--- src/main/java/com/android/tools/r8/utils/InternalOptions.java
+++ src/main/java/com/android/tools/r8/utils/InternalOptions.java
@@ -2517,14 +2517,15 @@ public class InternalOptions implements GlobalKeepInfoConfiguration {
   // the symbolic reference java.lang.Object.
   //
   // The support was added in Android O, however at least for j.l.CharSequence.equals the handling
-  // in Art was incorrect (b/231450655).
+  // in Art was incorrect (b/231450655). Further down the line b/271408544 indicate that it is not
+  // until Android S that this is working.
   //
   // javac started generating code like this with the fix for JDK-8272564, which will be part of
   // JDK 18.
   //
   // See b/218298666.
   public boolean canHaveInvokeInterfaceToObjectMethodBug() {
-    return isGeneratingDex() && getMinApiLevel().isLessThan(AndroidApiLevel.P);
+    return isGeneratingDex() && getMinApiLevel().isLessThan(AndroidApiLevel.S);
   }
 
   // b/238399429 Some art 6 vms have issues with multiple monitors in the same method
