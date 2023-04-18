fc8e281fdc4c025290a8aeb918514bb33a037642
Søren Gjesse <sgjesse@google.com>
2023-04-18 08:37:15 +0200

    Version 8.1.36
    Promote version 8.1.36-dev to 8.1.36.
    
    Release branch for Android Studio 2022.3.1 (Giraffe) / AGP 8.1.
    
    Change-Id: I32094b655b77d014c99847357fcf76d0b970ac49

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.1.36-dev";
+  public static final String LABEL = "8.1.36";
 
   private Version() {
   }
